6cbd2f9a31c1f4c6c333e45c7c40c18ccd8e7699
SihamAnanzeh <99133810+SihamAnanzh@users.noreply.github.com>
2022-06-16 14:21:49 +0300

    'fix

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,9 @@
 import { ThreeDots } from "react-loader-spinner";
-import { Random } from "react-animated-text";
-import { Wave } from "react-animated-text";
-import Head from "next";
 export default function Home() {
   return (
-    <>
-      <div className="section">
-        <h1>
-          <Wave text="Under Construction" effect="jump" effectChange={0.1} />
-        </h1>
-        <ThreeDots color="#fff" height={100} width={100} />
-      </div>
-    </>
+    <div className="section">
+      <h1>Under Construction</h1>
+      <ThreeDots color="#fff" height={100} width={100} />
+    </div>
   );
 }
